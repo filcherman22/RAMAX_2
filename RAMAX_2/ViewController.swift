4b55d79836b3ec2bc71e9c8cb8e14e7d21329fdc
Filipp <filipp.romano@gmail.com>
2019-07-10 22:39:16 +0300

    Initial Commit

---
+++ RAMAX_2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RAMAX_2
+//
+//  Created by Филипп on 10.07.2019.
+//  Copyright © 2019 Филипп. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
